bd3342887ae44918d3bee1c0ecae4d7982502cb1
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-04-04 09:58:48 -0300

    :sparkles: feat: Pasta e Componetes
    - Organizado estrutura de algumas pastas

--- shark-bank/src/pages/Home/index.jsx
+++ shark-bank/src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import { Container } from './styles';
-import { Button } from '../../components/Button';
+import { ButtonPrimario } from '../../components/Button/Primario';
 import { Input } from '../../components/Input';
 
 
@@ -7,7 +7,7 @@ export function Home() {
   return (
     <Container>
       <h1>Estilo botão componente</h1>
-      <Button />
+      <ButtonPrimario />
       <div style={{ backgroundColor: '#D9D9D9' }}>
         <h1>Estilo input componente</h1>
         <Input />
