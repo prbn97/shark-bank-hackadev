010d68ac273e0743d340638a2cd22334940a34f5
mauriciomuniz <mauricio.santana2010@hotmail.com>
2023-04-05 18:01:01 -0300

    :sparkles: feat: segunda página pix

---
+++ shark-bank/src/pages/Transferencia/Pix/pix2.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+export function Pix2() {
+  const [campoTexto, setCampoTexto] = useState('');
+
+  return (
+    <div className="App">
+      <h1>Para quem você quer transferir esse valor?</h1>
+      <p>Encontre um contato na sua lista ou inicia uma nova transferência:</p>
+      <input type="text" value={campoTexto} onChange={(event) => setCampoTexto(event.target.value)} />
+      <br/>
+      <button>Próximo</button>
+    </div>
+  );
+}
