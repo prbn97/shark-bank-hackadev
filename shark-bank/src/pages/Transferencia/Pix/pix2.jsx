501a6e9ae625d9f0fb2f73095f5bc4daa3138983
mauriciomuniz <mauricio.santana2010@hotmail.com>
2023-04-05 23:02:15 -0300

    :bricks: ci: adicionando botão voltar e modificação className

--- shark-bank/src/pages/Transferencia/Pix/pix2.jsx
+++ shark-bank/src/pages/Transferencia/Pix/pix2.jsx
@@ -4,7 +4,9 @@ export function Pix2() {
   const [campoTexto, setCampoTexto] = useState('');
 
   return (
-    <div className="App">
+    <div className="pix2Campo">
+      <button>Voltar</button>
+        <br/>
       <h1>Para quem você quer transferir esse valor?</h1>
       <p>Encontre um contato na sua lista ou inicia uma nova transferência:</p>
       <input type="text" value={campoTexto} onChange={(event) => setCampoTexto(event.target.value)} />
