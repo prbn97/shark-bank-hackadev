ec4d1d36894f094322e414709e2ea97b46d1c423
mauriciomuniz <mauricio.santana2010@hotmail.com>
2023-04-05 23:00:08 -0300

    :sparkles: feat: Página de senha

---
+++ shark-bank/src/pages/Transferencia/Senha/senha.jsx
@@ -0,0 +1,43 @@
+import { useState, useRef } from "react";
+
+//falta css e estilizar 
+export function Senha() {
+  const [digitos, setDigits] = useState(["", "", "", ""]); // array com os dígitos
+  const refs = [useRef(), useRef(), useRef(), useRef()]; // array com as referências para os campos
+
+  function handleChange(e, index) {
+    const value = e.target.value;
+    if (isNaN(value) || value.length > 1) return; // aceita apenas 1 dígito
+    const novosDigitos = [...digitos];
+    novosDigitos[index] = value;
+    setDigits(novosDigitos);
+    if (index < 3) {
+      refs[index + 1].current.focus(); // muda para o próximo campo
+    }
+  }
+
+  function handleNext() {
+    const password = digitos.join(""); // une os dígitos em uma senha
+    console.log(password); // imprime a senha
+  }
+
+  return (
+    <div className="senhaCampo">
+        <button>Voltar</button>
+        <br/>
+        <span>Digite a senha de 4 digitos do seu cartão</span>
+        <br/>
+      {digitos.map((digit, index) => (
+        <input
+          key={index}
+          type="password"
+          value={digit}
+          onChange={(e) => handleChange(e, index)}
+          ref={refs[index]}
+        />
+      ))}
+      <br/>
+      <button onClick={handleNext}>Próximo</button>
+    </div>
+  );
+}
