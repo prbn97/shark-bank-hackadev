26148e275528062fdf726e897903f5c32d7b3db7
mauriciomuniz <mauricio.santana2010@hotmail.com>
2023-04-07 03:57:38 -0300

    :bricks: ci: modificando estrutura para estilizar

--- shark-bank/src/pages/Transferencia/Pix/pix2.jsx
+++ shark-bank/src/pages/Transferencia/Pix/pix2.jsx
@@ -7,11 +7,13 @@ export function Pix2() {
     <div className="pix2Campo">
       <button>Voltar</button>
         <br/>
+      <div className=''>
       <h1>Para quem você quer transferir esse valor?</h1>
       <p>Encontre um contato na sua lista ou inicia uma nova transferência:</p>
       <input type="text" value={campoTexto} onChange={(event) => setCampoTexto(event.target.value)} />
       <br/>
       <button>Próximo</button>
+      </div>
     </div>
   );
 }
