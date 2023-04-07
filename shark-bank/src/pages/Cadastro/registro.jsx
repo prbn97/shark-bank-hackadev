4fe7333c7077fd13d3e55514b87a7c921ffafcb6
mauriciomuniz <mauricio.santana2010@hotmail.com>
2023-04-07 19:21:31 -0300

    :sparkles: feat: criação e validação página registro

---
+++ shark-bank/src/pages/Cadastro/registro.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+
+export function Registro() {
+  const [cep, setCep] = useState("");
+  const [uf, setUf] = useState("");
+  const [endereco, setEndereco] = useState("");
+  const [numero, setNumero] = useState("");
+  const [complemento, setComplemento] = useState("");
+  const [bairro, setBairro] = useState("");
+
+  const handleCepChange = (event) => {
+    setCep(event.target.value);
+  };
+
+  const handleUfChange = (event) => {
+    setUf(event.target.value);
+  };
+
+  const handleEnderecoChange = (event) => {
+    setEndereco(event.target.value);
+  };
+
+  const handleNumeroChange = (event) => {
+    setNumero(event.target.value);
+  };
+
+  const handleComplementoChange = (event) => {
+    setComplemento(event.target.value);
+  };
+
+  const handleBairroChange = (event) => {
+    setBairro(event.target.value);
+  };
+
+  const handleRegistrarClick = () => {
+    const registro = {
+      cep: cep,
+      uf: uf,
+      endereco: endereco,
+      numero: numero,
+      complemento: complemento,
+      bairro: bairro,
+    };
+    localStorage.setItem("registro", JSON.stringify(registro));
+  };
+
+  return (
+    <div className="corpo">
+      <div className="user">
+        <h1>Registro</h1>
+      </div>
+      <div className="campo">
+        <label>
+          CEP:
+          <input type="text" value={cep} onChange={handleCepChange} />
+        </label>
+        
+        <label>
+          UF:
+          <input type="text" value={uf} onChange={handleUfChange} />
+        </label>
+        <br/>
+        <label>
+          Endereço:
+          <input type="text" value={endereco} onChange={handleEnderecoChange} />
+        </label>
+        
+        <label>
+          Número:
+          <input type="text" value={numero} onChange={handleNumeroChange} />
+        </label>
+        <br/>
+        <label>
+          Complemento:
+          <input type="text" value={complemento} onChange={handleComplementoChange} />
+        </label>
+        <br/>
+        <label>
+          Bairro:
+          <input type="text" value={bairro} onChange={handleBairroChange} />
+        </label>
+      </div>
+      <button onClick={handleRegistrarClick}>Registrar</button>
+    </div>
+  );
+}
