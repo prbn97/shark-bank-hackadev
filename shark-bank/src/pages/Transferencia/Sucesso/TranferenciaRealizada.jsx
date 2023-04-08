0c0028ac2b14ad5d6af656dde1140be068ad2c02
PedroCruz000 <112138392+PedroCruz000@users.noreply.github.com>
2023-04-08 00:48:53 -0300

    :sparkles: feat: Pagina de transferência realizada
    Foi criada a pagina de confirmação de transferência, utilizando os botões com seus respectivos componentes

---
+++ shark-bank/src/pages/Transferencia/Sucesso/TranferenciaRealizada.jsx
@@ -0,0 +1,32 @@
+import { ButtonSair } from "../../../components/Button/Sair";
+import { ButtonAvancar } from "../../../components/Button/Avancar";
+import happyshark from "../../../assets/happyshark.png"
+
+export function TransferenciaRealizada(){
+
+
+    return(
+        <div>
+            <div>
+            <ButtonSair></ButtonSair>
+            </div>
+            
+            <div>
+                
+                <img src={happyshark} alt="logo de um tubarao sorridente" />
+                <div>
+                    <h1>Transferência realizada!</h1>
+                    <p>Para mais detalhes veja o histórico de transação da sua conta</p>
+                </div>
+            </div>
+
+            <ButtonAvancar></ButtonAvancar>
+
+        </div>
+        
+        
+        
+        
+    );
+
+}
