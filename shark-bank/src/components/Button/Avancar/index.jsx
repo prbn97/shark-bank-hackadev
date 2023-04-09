a4b476ce21d5b47d85a6efa986a92f8c32222ce9
mauriciomuniz <mauricio.santana2010@hotmail.com>
2023-04-09 14:57:28 -0300

    :recycle: refactor: componente avancar

--- shark-bank/src/components/Button/Avancar/index.jsx
+++ shark-bank/src/components/Button/Avancar/index.jsx
@@ -1,9 +1,9 @@
 import { Container } from "./styles";
 import svgIcon from "./avancar.svg";
 
-export function ButtonAvancar() {
+export function ButtonAvancar(props) {
     return (
-        <Container type="button" style={{ backgroundImage: `url(${svgIcon})` }}>
+        <Container type="button" style={{ backgroundImage: `url(${svgIcon})` }}onClick={props.onClick}>
         </Container>
     );
 }
