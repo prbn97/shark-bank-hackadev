04d0325d30272a332f03941b914509351b2ba827
mauriciomuniz <mauricio.santana2010@hotmail.com>
2023-04-09 15:04:35 -0300

    :recycle: refactor: transferencia

--- shark-bank/src/pages/Transferencia/Transferencia/transferencia2.jsx
+++ shark-bank/src/pages/Transferencia/Transferencia/transferencia2.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from 'react';
 import { ButtonRecuar } from '../../../components/Button/Recuar';
 import { ButtonAvancar } from '../../../components/Button/Avancar';
+import { Link } from 'react-router-dom';
 
 export function Transferencia2() {
   const [campoBanco, setCampoBanco] = useState('');
   const [campoAgencia, setCampoAgencia] = useState('');
   const [campoConta, setCampoConta] = useState('');
 
+  function handleAvancarClick() {
+    localStorage.setItem('transferencia2', JSON.stringify({
+      banco: campoBanco,
+      agencia: campoAgencia,
+      conta: campoConta,
+    }));
+  }
+  
   return (
     <div className="corpo">
-      <ButtonRecuar/>      
+      <Link to="/transferencia1">
+      <ButtonRecuar/>  
+      </Link>
+          
         <br/>
        <div className='campo'>
         <h1>Para quem você quer transferir esse valor?</h1>
@@ -22,7 +34,11 @@ export function Transferencia2() {
         <input type="text" value={campoAgencia} onChange={(event) => setCampoAgencia(event.target.value)} />
         <input type="text" value={campoConta} onChange={(event) => setCampoConta(event.target.value)} />
         <br/>
-        <ButtonAvancar/>       
+
+        <Link to="/transferencia3">
+        <ButtonAvancar onClick={handleAvancarClick}/>  
+        </Link>
+
       </div> 
     </div>
   );
