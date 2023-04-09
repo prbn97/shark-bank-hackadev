294e5c6527da061e071cf2453f6a2b2dec071061
mauriciomuniz <mauricio.santana2010@hotmail.com>
2023-04-09 14:58:12 -0300

    :recycle: refactor: componente

--- shark-bank/src/components/Lobby/Transferir/index.jsx
+++ shark-bank/src/components/Lobby/Transferir/index.jsx
@@ -1,7 +1,7 @@
 import { Container } from "./styles";
-import svgIcon from "./pix.svg";
+import svgIcon from "./transferir.svg";
 
-export function ButtonPix() {
+export function ButtonTrans() {
     return (
         <Container type="button" style={{ backgroundImage: `url(${svgIcon})` }}>
         </Container>
