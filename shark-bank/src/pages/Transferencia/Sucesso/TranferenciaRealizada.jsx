852dcb5536faab911557f2213ca3a235aa1c39a6
PedroCruz000 <112138392+PedroCruz000@users.noreply.github.com>
2023-04-09 19:18:28 -0300

    :sparkles: feat: Sucesso
    Feita a estilização da página de transferência realizada

--- shark-bank/src/pages/Transferencia/Sucesso/TranferenciaRealizada.jsx
+++ shark-bank/src/pages/Transferencia/Sucesso/TranferenciaRealizada.jsx
@@ -2,28 +2,41 @@ import { ButtonSair } from "../../../components/Button/Sair";
 import { ButtonAvancar } from "../../../components/Button/Avancar";
 import happyshark from "../../../assets/happyshark.png"
 import { Link } from 'react-router-dom';
+import Bateria from '../../../assets/bateria.png'
 
 export function TransferenciaRealizada(){
 
 
     return(
-        <div>
-            <div>
-            <ButtonSair/>
-            </div>
+        <div className="corpo">
+
+            <img src={Bateria} alt="imagem da bateria do celular" />
+
             
-            <div>
-                
-                <img src={happyshark} alt="logo de um tubarao sorridente" />
-                <div>
-                    <h1>Transferência realizada!</h1>
-                    <p>Para mais detalhes veja o histórico de transação da sua conta</p>
+            <div className='campo'>
+                <fieldset id="fields-set">
+
+                    <div>
+                        <ButtonSair/>
+                    </div>
+                    <div className="transferenciaRealizadaContainer">
+
+                        <img className="imagemTubaraoSorridente" src={happyshark} alt="logo de um tubarao sorridente" />
+                        <div className="transferenciaRealizadaBox">
+                            <h1>Transferência realizada!</h1>
+                            <p>Para mais detalhes veja o histórico de transação da sua conta</p>
+                        </div>
+
+                    </div>
+                </fieldset>
+
+                <div className='botaoAvancar'>
+                    <Link to="/principal">
+                        <ButtonAvancar/>
+                    </Link>
                 </div>
             </div>
 
-            <Link to="/principal">
-                 <ButtonAvancar/>
-            </Link>
             
 
         </div>
