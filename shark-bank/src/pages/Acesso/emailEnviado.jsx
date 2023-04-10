312fe20add07bebae8e0ea9482f330fffd96ee0c
mauriciomuniz <mauricio.santana2010@hotmail.com>
2023-04-10 11:23:03 -0300

    :recycle: refactor: finalizado email enviado
    terminado estilização email enviado
    
    Co-Authored-By: Pedro Cruz  <112138392+PedroCruz000@users.noreply.github.com>
    Co-Authored-By: Paulo Reis <110939784+prbn97@users.noreply.github.com>

--- shark-bank/src/pages/Acesso/emailEnviado.jsx
+++ shark-bank/src/pages/Acesso/emailEnviado.jsx
@@ -1,24 +1,30 @@
-
 import { Link } from 'react-router-dom';
+import { Button } from "../../components/Button";
+import Bateria from '../../assets/bateria.png'
 
 export function EmailEnviado() {
 
   return (
     <div className="corpo">
+      <img src={Bateria} alt="imagem da bateria do celular" />
 
-      <div className="user">
+      <div className="textoAcesso">
         <h1>Email enviado</h1>
       </div>
       
-      <div className="campo">
+      <div className="campoAcesso">
       
-      <p>
+      <p className='textoPequenoAcesso'>
       Foi enviado um e-mail para sua caixa de entrada, siga as instruções para a 
       recuperação da sua senha.
       </p>
-      <Link to="/login">
-          <button >Voltar</button>
-      </Link>
+
+      <div className="buttonsAcesso" >
+        <Link to="/login">
+            <Button titulo = "Voltar"/>
+        </Link>
+      </div>
+      
       </div>
       
     </div>
