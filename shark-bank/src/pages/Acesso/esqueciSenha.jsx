d40a6ecbb99dd97bc7e98e5fde84dd34ef4cba2d
PedroCruz000 <112138392+PedroCruz000@users.noreply.github.com>
2023-04-10 01:34:49 -0300

    :lipstick: feat: Pagina esqueci a senha
    foi criado a estilização da pagina esqueci a senha

--- shark-bank/src/pages/Acesso/esqueciSenha.jsx
+++ shark-bank/src/pages/Acesso/esqueciSenha.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { Link } from 'react-router-dom';
+import Bateria from '../../assets/bateria.png'
+import { Button } from "../../components/Button";
+
 
 export function EsqueciSenha() {
   const [email, setEmail] = useState("");
@@ -13,35 +16,63 @@ export function EsqueciSenha() {
   return (
     <div className="corpo">
 
-      <div className="user">
-        <h1>Esqueci a senha</h1>
+      <img src={Bateria} alt="imagem da bateria do celular" />
+
+      <div className="textoAcesso">
+        <h2>Esqueci a senha</h2>
       </div>
       
-      <div className="campo">
-      <form onSubmit={handleSubmit}>
-      <p>
-        Para redefinir a sua senha, informe o e-mail cadastrado na sua conta e
-        lhe enviaremos um link com as instruções.
-      </p>
-        <label htmlFor="email">E-mail:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          required
-        />
-        <br />
-        
-      </form>
-      <Link to="/emailenviado">
-        <button type="submit">Enviar</button>
-      </Link>
-      
+      <div className="campoAcesso">
 
-      <Link to="/login">
-        <button>Voltar</button>
-      </Link>
+        <div>
+          
+          <p className="textoPequenoAcesso">
+            Para redefinir a sua senha, informe o e-mail cadastrado na sua conta e
+            lhe enviaremos um link com as instruções.
+          </p>
+
+        </div>
+
+
+        <div className="imputAcesso">
+          
+          <label className="inputNome" htmlFor="email">E-mail:</label>
+          <input className="input"
+            type="email"
+            id="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+
+        </div>
+
+        <div className="buttonsAcesso">
+
+
+          <Link to="/emailenviado">
+            <Button titulo= 'Enviar' />
+            
+          </Link>
+
+          
+          
+
+          <Link to="/login">
+            <Button 
+            titulo= 'Voltar'             
+            corTitulo='#101626'
+            corFundo='linear-gradient(180deg, #EBEBEB 24.32%, #101626 120.61%)'/>
+            
+          </Link>
+
+        </div>
+
+
+        <form onSubmit={handleSubmit}>
+          
+          
+        </form>
       
       </div>
     </div>
