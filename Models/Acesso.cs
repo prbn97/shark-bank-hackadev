2aeed5ab2bce300b4b5f68fd9b1c0ef883ca45ff
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-04-19 11:55:21 -0300

    :triangular_flag_on_post: Flag: Funcionando implementação
    Para a criação de codigo com codegenerator é obrigatório o uso da variável "Id"como primeira propriedade da entidade.
    
    Co-Authored-By: Mauricio Santana <29042923+mauriciomuniz@users.noreply.github.com>

---
+++ Models/Acesso.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace sharp_coders_hackadev.Models;
+
+public class Acesso
+{
+    public string id { get; set; }
+    public string cpf_acesso { get; set; }
+    public string senha_acesso { get; set; }
+}
