2aeeb3e14a4bf9430cce9560f1b2c172b16d7924
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-04-22 12:06:52 -0300

    :lipstick: Aplicando estilizção no Home Page
    :sparkles: Padronizando a forma de estilização

--- ClientApp/src/pages/Home/styles.js
+++ ClientApp/src/pages/Home/styles.js
@@ -2,26 +2,50 @@ import styled from "styled-components";
 import imagemOnda from "../../assets/images/onda.svg";
 
 export const Container = styled.div`
-  display: flex;
   align-items: center;
+  display: flex;
+  gap: 9em;
+  height: 100vh;
   justify-content: center;
   margin: 0;
-`;
 
-export const ContainerContent = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+  .container_content {
+    background-color: green;
+  }
+
+  .container_painel {
+    background-color: green;
+    background: #D9D9D9;
+    box-shadow: 0px 4px 18px rgba(0, 0, 0, 0.25);
+    border-radius: 26px;
+  }
+
+  .container_painel_content {
+    background-color: yellow;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    /* gap: 20px; */
+  }
+
+  .container_painel_buttons {
+    background-color: blue;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 20px;
+  }
 `;
 
 export const Title = styled.h1`
 `;
 
-export const ImagemOnda = styled.div`
+export const ImagemOnda = styled.img`
   height: 10%;
   width: 30%;
-  background: url(${imagemOnda}) no-repeat;
+  src: url(${imagemOnda});
   background-size: contain;
   background-position: center;
 `;
@@ -29,34 +53,9 @@ export const ImagemOnda = styled.div`
 export const Texto = styled.p`
 `;
 
-export const ContainerPainel = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  background: #D9D9D9;
-  box-shadow: 0px 4px 18px rgba(0, 0, 0, 0.25);
-  border-radius: 26px;
-`;
-
-export const ContainerPainelContent = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  gap: 20px;
-`;
-
 export const ContainerPainelContentTitle = styled.h2`
-`;
+  `;
 
 export const ContainerPainelContentText = styled.p`
-`;
+  `;
 
-export const ContainerPainelButton = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  gap: 20px;
-`;
