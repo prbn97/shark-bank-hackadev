29c258e4bede15278506153c4befc5a5c73acea0
Paulo Reis <PAULOREIS.RJ97@GMAIL.COM>
2023-04-22 17:20:34 -0300

    :sparkles: Feat: Criação da estrutura da página
    :sparkles: criação da estrutura html
    :sparkles: adição da imagem do layout
    
    Co-Authored-By: Julianacris <118282753+Julianacris@users.noreply.github.com>

--- ClientApp/src/pages/Redefinir/index.js
+++ ClientApp/src/pages/Redefinir/index.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import * as C from './styles'
+import Input from '../../components/Input'
+
+import * as S from '../../components/Botao/styles';
+
+import tubaraoConversando1 from '../../assets/images/tubarao-conversando.png'
+
 
 function RedefinirSenha() {
   return (
-    <div>
-      <h1 color='green'>RedefinirSenha</h1>
-    </div>
+    <C.Container>
+
+      <div class="mainContent-style">
+
+        <img src={tubaraoConversando1} alt=''/>
+
+        <C.Form>
+          <C.FormTitle>
+          Redefinir senhas
+          </C.FormTitle>
+
+          <C.FieldSet>
+            <div class="fieldSet_text">
+            Para redefinir a sua senha, informe o e-mail cadastrado na sua e lhe enviaremos um link com as intruções.
+            </div>
+
+            <Input
+            type="email"
+            placeholder="Digite aqui o seu e-mail"/>
+
+
+            <S.ButtonPrimario text="Cadastrar" onClick={() => [alert("Cadastro feito com sucesso?! *Fazer lógica para tela de acesso")]}>Cadastrar</S.ButtonPrimario>
+            <S.ButtonSecundario text="Cadastrar" onClick={() => [alert("Cadastro feito com sucesso?! *Fazer lógica para tela de acesso")]}>Cadastrar</S.ButtonSecundario>
+          </C.FieldSet>
+        </C.Form>
+        
+      </div>
+      
+    </C.Container>
   )
 }
 
---
+++ ClientApp/src/pages/Redefinir/styles.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+
+
+export const Container = styled.div`
+  .mainContent-style{
+  display: flex;
+  width: 984px;
+  height: 472px;
+  margin: auto;
+  margin-top: 248px;
+}
+`;
+
+
+export const Form = styled.div`
+  min-width: 343px;
+  height: 708px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+
+`;
+
+export const FormTitle = styled.div`
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 44px;
+
+  color: #EBEBEB;
+`;
+
+export const FieldSet = styled.div`
+  width: 343px;
+  height: 344px;
+  padding: 24px;
+
+  background: #D9D9D9;
+  border-radius: 26px;
+
+`;
+
+
