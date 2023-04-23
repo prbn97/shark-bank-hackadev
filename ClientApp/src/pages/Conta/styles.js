5b1d013dd006a2ba6fd7bb9a9f039e79931de059
Paulo Reis <PAULOREIS.RJ97@GMAIL.COM>
2023-04-22 23:59:52 -0300

    :sparkles: feat:Criação da tela Conta (lobby)
    :sparkles: criação dos primeiros elementos baseado no figma
    :sparkles: criação de link para o historico de transações

--- ClientApp/src/pages/Conta/styles.js
+++ ClientApp/src/pages/Conta/styles.js
@@ -7,21 +7,47 @@ export const Container = styled.div`
   flex-direction: column;
   height: 100vh;
   gap: 20px;
-
-  
-  @media screen and (max-width: 760px) {
-    
-  }
-
 `;
 
-export const Title = styled.h1`
+export const User = styled.div`
+display: flex;
+align-items: center;
+gap: 8px;
+.UserName{
+font-weight: 700;
+font-size: 18px;
+color: white;
+}
+`;
+export const Saldo = styled.div`
+
+font-weight: 700;
+font-size: 16px;
+color: #E6E6E6;
+margin: 36px auto 24px;
+
+.SaldoLabel{ 
+  display: flex;
+  gap: 128px;
+  font-weight: 700;
+  font-size: 16px;
+ }
+
+.Saldo_link-extrato a{
+font-weight: 700;
+font-size: 16px;
+color: #E6E6E6; 
+}
+
+.SaldoOutput-style input{
+  margin-top: 16px;
+  height: 56px;
+  width: 100%;
+  background: linear-gradient(112.48deg, rgba(255, 255, 255, 0.3) 6.31%, rgba(255, 255, 255, 0) 98.51%);
+  border-radius: 26px;
+}
 
 `;
 
 export const ContainerBotoes = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 50px;
-  background: purple;
 `;
