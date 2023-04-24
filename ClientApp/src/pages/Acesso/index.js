73a87d45f67c90fe6fa69e42d7e342ccf03f9dbc
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-04-24 00:26:54 -0300

    :bug: Fix: Pagina de acesso
    Corrigido problema de botao e tags

--- ClientApp/src/pages/Acesso/index.js
+++ ClientApp/src/pages/Acesso/index.js
@@ -5,7 +5,6 @@ import useAuth from '../../hooks/useAuth';
 import * as C from './styles'
 
 import Input from '../../components/Input';
-import Botao from '../../components/Botao';
 
 import imagemTubarao from '../../assets/images/tubarao-dando-ola.svg'
 
@@ -37,53 +36,52 @@ const Acesso = () => {
 
   return (
     <C.Container>
-        <C.containerContent>
-          
-          <C.LogoTubarao src={imagemTubarao} alt="Tubarão dando olá" />
-          <C.containerLabel>
+      <C.ContainerContent>
 
-            <C.Label>Acesso</C.Label>
-            <C.Content>
-              <Input
-                type="text"
-                placeholder="Digite aqui o seu CPF"
-                value={cpf}
-                onChange={(error) => [setCPF(error.target.value), setError("")]}
-                />
-              <Input
-                type="password"
-                placeholder="Digite aqui a sua senha"
-                value={senha}
-                
-                onChange={(error) => [setSenha(error.target.value), setError("")]}
-                />
-              <C.LabelError>{error}</C.LabelError>
+        <C.LogoTubarao src={imagemTubarao} alt="Tubarão dando olá" />
+        <C.containerLabel>
 
-              <C.ContentAccountPassword>
+          <C.Label>Acesso</C.Label>
+          <C.Content>
+            <Input
+              type="text"
+              placeholder="Digite aqui o seu CPF"
+              value={cpf}
+              onChange={(error) => [setCPF(error.target.value), setError("")]}
+            />
+            <Input
+              type="password"
+              placeholder="Digite aqui a sua senha"
+              value={senha}
 
-                <C.RememberAccount>
-                  <input type='checkbox' name="Lembrar conta"></input>
-                  <p className='TextRememberAccount'>Lembrar conta </p>
+              onChange={(error) => [setSenha(error.target.value), setError("")]}
+            />
+            <C.LabelError>{error}</C.LabelError>
 
-                </C.RememberAccount>
+            <C.ContentAccountPassword>
 
-                <C.ForgetPassword>
-                  <a> Esqueci a senha</a>
+              <C.RememberAccount>
+                <input type='checkbox' name="Lembrar conta"></input>
+                <p className='TextRememberAccount'>Lembrar conta </p>
 
-                </C.ForgetPassword>
+              </C.RememberAccount>
 
-              </C.ContentAccountPassword>
-              <S.ButtonPrimario>Acessar</S.ButtonPrimario>
-              <C.LabelCadastro>
-                Ainda não é cliente?
-                <C.Strong>
-                  <Link to="/cadastro">&nbsp;Cadastre-se</Link>
-                </C.Strong>
-              </C.LabelCadastro>
-            </C.Content>
-          </C.containerLabel>
+              <C.ForgetPassword>
+                <Link to="/redefinir">&nbsp; Esqueci a senha</Link>
+              </C.ForgetPassword>
 
-        </C.containerContent>
+            </C.ContentAccountPassword>
+            <S.ButtonPrimario text="Acessar" onClick={handleAcesso}>Acessar</S.ButtonPrimario>
+            <C.LabelCadastro>
+              Ainda não é cliente?
+              <C.Strong>
+                <Link to="/cadastro">&nbsp;Cadastre-se</Link>
+              </C.Strong>
+            </C.LabelCadastro>
+          </C.Content>
+        </C.containerLabel>
+
+      </C.ContainerContent>
     </C.Container>
   );
 };
